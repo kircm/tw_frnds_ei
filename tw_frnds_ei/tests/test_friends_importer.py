2f3aac7347c68924a310da5cce473801dc090094
kircm <kircmarc@gmail.com>
2020-10-09 06:26:53 -0400

    Changed test csv file names to not be .ignored

--- tw_frnds_ei/tests/test_friends_importer.py
+++ tw_frnds_ei/tests/test_friends_importer.py
@@ -14,7 +14,7 @@ def test_importer_fails_csv_file_bad(tw_client_ok):
     logger.info("---------- test_importer_fails_csv_file_bad ----------")
     user_name = "bad_csv"
     mock_client = tw_client_ok(user_name)
-    importer = FriendsImporter(mock_client, IMP_DATA_DIR, "bad_csv.csv")
+    importer = FriendsImporter(mock_client, IMP_DATA_DIR, "bad_csv.test_csv")
 
     ok, msg, friendships_remaining = importer.process()
 
@@ -28,7 +28,7 @@ def test_importer_fails_csv_file_too_big(tw_client_ok):
     logger.info("---------- test_importer_fails_csv_file_too_big ----------")
     user_name = "csv_too_big"
     mock_client = tw_client_ok(user_name)
-    importer = FriendsImporter(mock_client, IMP_DATA_DIR, "csv_too_big.csv")
+    importer = FriendsImporter(mock_client, IMP_DATA_DIR, "csv_too_big.test_csv")
     importer.MAX_CSV_ROWS = 10
 
     ok, msg, friendships_remaining = importer.process()
@@ -43,7 +43,7 @@ def test_importer_fails_csv_empty(tw_client_ok):
     logger.info("---------- test_importer_fails_csv_empty ----------")
     user_name = "empty_csv"
     mock_client = tw_client_ok(user_name)
-    importer = FriendsImporter(mock_client, IMP_DATA_DIR, "empty_csv.csv")
+    importer = FriendsImporter(mock_client, IMP_DATA_DIR, "empty_csv.test_csv")
 
     ok, msg, friendships_remaining = importer.process()
 
@@ -57,7 +57,7 @@ def test_importer_imports_ok(tw_client_ok):
     logger.info("---------- test_importer_imports_ok ----------")
     user_name = "importing_user"
     mock_client = tw_client_ok(user_name)
-    importer = FriendsImporter(mock_client, IMP_DATA_DIR, "good_csv.csv")
+    importer = FriendsImporter(mock_client, IMP_DATA_DIR, "good_csv.test_csv")
 
     ok, msg, friendships_remaining = importer.process()
 
@@ -73,7 +73,7 @@ def test_importer_retries_ok(tw_client_ok_retries):
     user_name = "retry_user"
     user_id_err = 12349  # this user id will fail in the mock twython client
     mock_client = tw_client_ok_retries(user_name, user_id_err=user_id_err)
-    importer = FriendsImporter(mock_client, IMP_DATA_DIR, "good_csv.csv")
+    importer = FriendsImporter(mock_client, IMP_DATA_DIR, "good_csv.test_csv")
     importer.RETRY_SLEEP_CHECK_EVERY_SECS = 4
     importer.RETRY_SHORT_SECONDS_TO_WAIT = 6
     importer.RETRY_LONG_SECONDS_TO_WAIT = 10
@@ -91,7 +91,7 @@ def test_importer_irrecoverable_twitter_err(tw_client_nok):
     user_name = "erroring_user"
     user_id_err = 12349  # this user id will fail in the mock twython client
     mock_client = tw_client_nok(user_name, user_id_err=user_id_err)
-    importer = FriendsImporter(mock_client, IMP_DATA_DIR, "good_csv.csv")
+    importer = FriendsImporter(mock_client, IMP_DATA_DIR, "good_csv.test_csv")
     importer.RETRY_SLEEP_CHECK_EVERY_SECS = 4
     importer.RETRY_SHORT_SECONDS_TO_WAIT = 6
     importer.RETRY_LONG_SECONDS_TO_WAIT = 10
