c81da5c43588fc6e9888faa6a3fa07345cda0f83
kircm <kircmarc@gmail.com>
2020-12-03 18:28:34 +0100

    Read .env file finding its location relative to the config_app.py script

--- tw_frnds_ei/config_app.py
+++ tw_frnds_ei/config_app.py
@@ -1,7 +1,10 @@
 import configparser
+import pathlib
+
+dot_env_file_path = pathlib.Path(__file__).resolve().parents[1].joinpath('.env')
 
 config = configparser.ConfigParser()
-config.read_file(open('.env'))
+config.read_file(open(dot_env_file_path))
 env_config = config['DEFAULT']
 
 MAX_NUM_FRIENDS = 3000
